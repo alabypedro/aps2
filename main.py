c6ee36122cc0581b73cd9577981018607ca0c4f7
l1nk404 <gabrielhmorais98@gmal.com>
2024-10-22 19:58:10 -0300

    showDB class

--- main.py
+++ main.py
@@ -21,7 +21,61 @@ class Color:
     END = '\033[0m'
 
 
-# Functions --------------------------------------------------------
+class ShowDB:
+    def __init__(self, data):
+        self.expand_db = pd.json_normalize(data['Cidades'])
+        self.metropolis = self.expand_db[self.expand_db['População'] > 10000000]
+        self.largeCities = self.expand_db[self.expand_db['Área'] > 1000]
+
+    # Print functions
+    def print_cities(self):
+        print(self.expand_db)
+
+    def print_metropolis(self):
+        print(self.metropolis)
+
+    def print_largeCities(self):
+        print(self.largeCities)
+
+    # Sort Options
+    def handle_sorted_options(self, current_option):
+        print("\n[1] A-Z [2] Área (km²) [3] População [4] Índice [0] 0 para voltar")
+        user_input = input("Organizar por: ")
+
+        # Handle the "Visualizar todas cidades" option
+        if current_option == 1:
+            # Variables that handle the sorted result
+            self.sortName = self.expand_db.sort_values(
+                by="Cidade", ascending=True)
+            self.sortArea = self.expand_db.sort_values(
+                by="Área", ascending=False)
+            self.sortPop = self.expand_db.sort_values(
+                by="População", ascending=False)
+            self.sortIndex = self.expand_db.sort_index(ascending=True)
+
+            if user_input == '1':
+                tools.clear()
+                print(self.sortName)
+            elif user_input == '2':
+                tools.clear()
+                print(self.sortArea)
+            elif user_input == '3':
+                tools.clear()
+                print(self.sortPop)
+            elif user_input == '4':
+                tools.clear()
+                print(self.sortIndex)
+            elif user_input == '0':
+                return visualize_db()
+            else:
+                tools.input_error(user_input)
+
+        if current_option == 2:
+            ...
+        if current_option == 3:
+            ...
+
+            # Functions --------------------------------------------------------
 
 
 def panda_db():
@@ -36,14 +90,14 @@ def panda_db():
 
 def visualize_db():
     tools.clear()
-    DataBase = panda_db()
+    DataBase = ShowDB(panda_db())
 
     tools.session_header("Base de Dados")
 
     options_display = """
 [1] Visualizar todos as cidades
-[2] Buscar por cidades
-[3]
+[2] Visualizar apenas metrópoles
+[3] Visualizar apenas cidades com área > 1000 km²
 [0] Voltar
 [s] Sair
 """
@@ -52,27 +106,30 @@ def visualize_db():
     user_input = input("Escolha uma opção: ")
 
     if user_input == '1':
-        ...
+        tools.clear()
+        DataBase.print_cities()
+        while True:
+            DataBase.handle_sorted_options(1)
+
     elif user_input == '2':
+        DataBase.print_metropolis()
+        # display_1 = "[1] A-Z [2] Tamanho [3] Área (km²) [0] 0 para voltar"
+        # user_input = input("Organizar por: ")
         ...
     elif user_input == '3':
-        ...
+        DataBase.print_largeCities()
     elif user_input == '0':
         main_menu()
     elif user_input.lower() == 's':
         exit(0)
         print("Até mais!")
 
-    # # Expand the db and print in collums and row
-    # DataBase_expanded = pd.json_normalize(DataBase['Cidades'])
-    # print(DataBase_expanded)
-
 
 def main_menu():
     tools.session_header("Menu Principal")
 
     options_display = """
-[1] Acessar base de dados
+[1] Visualizar base de dados
 [2] Procurar por cidade específica
 [3] Inserir uma nova cidade
 [4] Alterar dados
