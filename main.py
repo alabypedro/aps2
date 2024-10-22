a836a7a082461179e9b25cae440a10c0921adca7
l1nk404 <gabrielhmorais98@gmal.com>
2024-10-22 15:58:48 -0300

    first commit

--- main.py
+++ main.py
@@ -19,17 +19,23 @@ class PreMadeMessages:
             self.text,
             Color.END, sep="", end=2*"\n"
         )
+
+
 # Functions --------------------------------------------------------
 
 
+def db_update():
+    # Write the file path to the city_db.json on db directory
+    FILE_PATH = os.path.join("db", "city_db.json")
+
+    # Read the file and transfer to the pandas Data Base
+    with open(FILE_PATH, 'r', encoding='utf8') as db_file:
+        city_dict = json.load(db_file)
+        city_db = pandas.DataFrame(city_dict)
+
+    print(city_db)
+
+
 # Main -------------------------------------------------------------
-tools.clear()
 
-# Write the file path to the city_db.json on db directory
-FILE_PATH = os.path.join("db", "city_db.json")
-
-with open(FILE_PATH, 'r', encoding='utf8') as db_file:
-    city_dict = json.load(db_file)
-    city_db = pandas.DataFrame(city_dict)
-
-print(city_db)
+tools.clear()  # Clear the screen
