55fbfb0ece5aab3b6ed9e1fa0ae2d942896e5461
graziela <grazielalinda444@gmail.com>
2024-10-23 12:23:59 -0300

    exemplo

---
+++ exemplo.py
@@ -0,0 +1,35 @@
+import localPackage as tools
+cidade = ""
+area = ""
+populacao = ""
+
+while True:
+    tools.clear()
+    tools.session_header("exemplo")
+    
+    print(f"""
+[1] Cidade: {cidade}
+[2] Área: {area}
+[3] População: {populacao} 
+[4] Confirma
+[0] Voltar         
+""")
+    user_input=input("Selecione uma opção: ")
+
+    if user_input== "1":
+        cidade = input("Digite o nome da cidade: ")
+        
+    elif user_input== "2":
+        area = input("Digite a área da cidade: ")
+        
+    elif user_input== "3":
+        populacao = input("Digite a população da cidade: ")
+       
+    elif user_input == "4": 
+        ...  
+        
+    elif user_input== "0":
+        exit(0)
+        
+    else: 
+        tools.input_error(user_input)
